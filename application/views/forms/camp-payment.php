a9cd14bdf3597fefd19c24a5cddcea6d8ae4fc74
Vladimir Kharseev <vkharseev@gmail.com>
2013-08-31 14:03:34 +0400

    create

--- application/views/forms/camp-payment.php
+++ application/views/forms/camp-payment.php
@@ -1,9 +1,9 @@
 <form method="post" action="<?=site_url('camp/payment')?>" id="frm-payment">
-	<input class="valid-required"  placeholder="Название компании" name="ccompany" type="text" />
-	<input  placeholder="Фактический адрес" name="caddress1" type="text" />
-	<input class="valid-required"  placeholder="Юридический адрес" name="caddress2" type="text" />
-	<input class="valid-required"  placeholder="ИНН" name="cinn" type="text" />
-	<input  placeholder="КПП" name="ckpp" type="text" />
-	<input class="valid-phone-number valid-required"  placeholder="Основной телефон" name="cphone" type="text" />
+	<input autocomplete="off" class="valid-required"  placeholder="Название компании" name="ccompany" type="text" />
+	<input autocomplete="off" placeholder="Фактический адрес" name="caddress1" type="text" />
+	<input autocomplete="off" class="valid-required"  placeholder="Юридический адрес" name="caddress2" type="text" />
+	<input autocomplete="off" class="valid-required"  placeholder="ИНН" name="cinn" type="text" />
+	<input autocomplete="off" placeholder="КПП" name="ckpp" type="text" />
+	<input autocomplete="off" class="valid-phone-number valid-required"  placeholder="Основной телефон" name="cphone" type="text" />
 	<a href="#" id="payment" class="btn">Сформировать счет</a>
 </form>
--- application/views/forms/camp-register.php
+++ application/views/forms/camp-register.php
@@ -1,9 +1,9 @@
 <form method="post" action="<?=site_url('camp/register')?>" id="frm-register">
-	<input class="valid-email valid-required" placeholder="E-mail" name="email" type="text" />
-	<input class="valid-required"  placeholder="Фамилия" name="lname" type="text" />
-	<input class="valid-required"  placeholder="Имя" name="fname" type="text" />
-	<input class="valid-required"  placeholder="Место работы" name="company" type="text" />
-	<input class="valid-required"  placeholder="Должность" name="position" type="text" />
+	<input class="valid-email valid-required" autocomplete="off" placeholder="E-mail" name="email" type="text" />
+	<input class="valid-required" autocomplete="off" placeholder="Фамилия" name="lname" type="text" />
+	<input class="valid-required" autocomplete="off" placeholder="Имя" name="fname" type="text" />
+	<input class="valid-required" autocomplete="off" placeholder="Место работы" name="company" type="text" />
+	<input class="valid-required" autocomplete="off" placeholder="Должность" name="position" type="text" />
 	<input type="checkbox" value="1" name="subscribe" /> 
 	<label for="subscribe">Подписаться на будущие события</label>
 	<a href="#" id="rsubmit" class="btn">Зарегистрироваться</a>
