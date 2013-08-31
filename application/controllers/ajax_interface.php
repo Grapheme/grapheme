09e52fb6ed73c898328e17b324cb4e4d9ad1bd4d
Vladimir Kharseev <vkharseev@gmail.com>
2013-08-31 15:13:58 +0400

    create

--- application/controllers/ajax_interface.php
+++ application/controllers/ajax_interface.php
@@ -15,9 +15,22 @@ class Ajax_interface extends MY_Controller{
 		$json_request = array('status'=>FALSE,'responseText'=>'','numberOrder'=>0);
 		if($this->postDataValidation('camp_register') === TRUE):
 			if($orderID = $this->insertItem(array('insert'=>$this->input->post(),'model'=>'camp_register'))):
-				$post = $this->input->post();
-				$post['order_id'] = $orderID;
+				$mailData['order'] = $post = $this->input->post();
+				$mailData['order']['order_id'] = $post['order_id'] = $orderID;
 				$this->session->set_userdata('order_data',json_encode($post));
+				
+				$mailData['order']['summa'] = 2000; $mailData['order']['summa_word'] = 'две тясычи';
+				$now = time();
+				if($now >= strtotime('02.09.2013') && $now < strtotime('18.09.2013')):
+					$mailData['order']['summa'] = 3500; $mailData['order']['summa_word'] = 'три тясычи пятсот';
+				elseif($now >= strtotime('18.09.2013')):
+					$mailData['order']['summa'] = 4500; $mailData['order']['summa_word'] = 'четыре тясычи пятсот';
+				endif;
+				$mailtext = $this->load->view('mails/camp-order',$mailData,TRUE);
+				$this->sendMail($mailData['order']['email'],'be.digital@grapheme.ru','Графема','Мастер-класс Максима Костенко',$mailtext);
+				$mailtext = $this->load->view('mails/camp-order-admin',$mailData,TRUE);
+				$this->sendMail('be.digital@grapheme.ru','info@grapheme.ru','Графема','Мастер-класс Максима Костенко',$mailtext);
+				
 				$json_request['numberOrder'] = str_pad($orderID,3,'0',STR_PAD_LEFT).'/CAMP_1';
 				$json_request['status'] = TRUE;
 			endif;
@@ -38,18 +51,14 @@ class Ajax_interface extends MY_Controller{
 				$mailData['order'] = json_decode($this->session->userdata('order_data'),TRUE);
 				$mailData['payment'] = $this->input->post();
 				$mailData['payment']['id'] = $paymentID;
-				$summa = 2000; $summaWords = 'две тясычи';
+				$mailData['order']['summa'] = 2000; $mailData['order']['summa_word'] = 'две тясычи';
 				$now = time();
 				if($now >= strtotime('02.09.2013') && $now < strtotime('18.09.2013')):
-					$summa = 3500; $summaWords = 'три тясычи пятсот';
+					$mailData['order']['summa'] = 3500; $mailData['order']['summa_word'] = 'три тясычи пятсот';
 				elseif($now >= strtotime('18.09.2013')):
-					$summa = 4500; $summaWords = 'четыре тясычи пятсот';
+					$mailData['order']['summa'] = 4500; $mailData['order']['summa_word'] = 'четыре тясычи пятсот';
 				endif;
-				$mailData['payment']['summa'] = $summa;
-				$mailData['payment']['summa_word'] = $summaWords;
-				$mailtext = $this->load->view('mails/camp-order',$mailData,TRUE);
-				$this->sendMail($mailData['order']['email'],'be.digital@grapheme.ru','Графема','Мастер-класс Максима Костенко',$mailtext);
-				$mailtext = $this->load->view('mails/camp-order-admin',$mailData,TRUE);
+				$mailtext = $this->load->view('mails/camp-payment-admin',$mailData,TRUE);
 				$this->sendMail('be.digital@grapheme.ru','info@grapheme.ru','Графема','Мастер-класс Максима Костенко',$mailtext);
 				$this->session->unset_userdata('order_data');
 				$json_request['status'] = TRUE;
