b52e55ab4e74b251d12fa1c019b180c7ff15c9e4
Artem <reaper90fang@gmail.com>
2013-09-18 19:16:33 +0400

    typeKit onActive + media queries

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,6 @@
 <?php if(!defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['base_url'] 			= "http://grapheme/";
+$config['base_url'] 			= "http://grapheme.ru/";
 $config['index_page'] 			= '';
 $config['uri_protocol']			= 'AUTO';
 $config['url_suffix'] 			= '';
--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 function animateOnLoad() {
 	var $fTop = $('.promo h1');
 	var $fBot = $('.promo .company-desc');	
-	$fTop.animate({ opacity: '1' }, 800 ).css({'-webkit-transform': 'translate3d(0px, 0px, 0px)', '-ms-transform': 'translate(0px, 0px)', 'transform': 'translate(0px, 0px)'}); 
-	$fBot.animate({ opacity: '1' }, 800 ).css({'-webkit-transform': 'translate3d(0px, 0px, 0px)', '-ms-transform': 'translate(0px, 0px)', 'transform': 'translate(0px, 0px)'});	
+	var $sep = $('.promo .sep');
+	$fTop.animate({ opacity: '1' }, 1200 ).css({'-webkit-transform': 'translate3d(0px, 0px, 0px)', '-ms-transform': 'translate(0px, 0px)', 'transform': 'translate(0px, 0px)'}); 
+	$fBot.animate({ opacity: '1' }, 1200 ).css({'-webkit-transform': 'translate3d(0px, 0px, 0px)', '-ms-transform': 'translate(0px, 0px)', 'transform': 'translate(0px, 0px)'});	
+	$sep.animate({ opacity: '1' }, 1600 );
 }
+function adjustPromoHeight() {
+	var winHeight = $(window).height();
+	var headerHeight = $('header').height();
+	var footerHeight = $('footer').height();
+	var resizeBlock = $('article .promo');
+	resizeBlock.height( winHeight - ( footerHeight + headerHeight) );
+}
+$(document).ready( function(){
+	adjustPromoHeight();
+});
+$(window).resize( function(){
+	adjustPromoHeight();
+});
